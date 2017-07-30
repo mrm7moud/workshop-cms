275f51edef17133dfe1237bdf00e686a79839948
mrm7moud <mrm7moud@hotmail.com>
2017-07-30 23:33:25 +0300

    Finish code for the generic route

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 var http = require('http');
 var fs = require('fs');
 
+var ContentTypes = {
+    css: 'text/css',
+    js: 'application/javascript',
+    jpg: 'image/jpg',
+    ico: 'image/x-icon',
+    png: 'image/png'
+};
+
 var message = 'I am so happy to be part of the Node Girls workshop!';
 
 function handler(request, response) {
@@ -9,7 +17,9 @@ function handler(request, response) {
     //console.log(method);
 
     if (endpoint === '/') {
-        response.writeHead(200, { "Content-Type": "text/html" });
+        response.writeHead(200, {
+            "Content-Type": "text/html"
+        });
 
         fs.readFile(__dirname + '/../public/index.html', function (error, file) {
             if (error) {
@@ -21,20 +31,31 @@ function handler(request, response) {
         });
 
     } else if (endpoint === '/node') {
-        response.writeHead(200, {"Content-Type": "text/html"});
+        response.writeHead(200, {'Content-Type': 'text/html'});
         response.write(message); //response body
         response.end(); // finish response
     } else if (endpoint === '/girls') {
-        response.writeHead(200, {"Content-Type": "text/html"});
+        response.writeHead(200, {'Content-Type': 'text/html'});
         response.write(message); //response body
         response.end(); // finish response
     } else {
+        //var url = request.url;
+        var parts = endpoint.split('.'); 
+        // this line return 'main', 'css' and 'index', 'js'
+        //console.log(parts[0]);
+        var fileExtension = parts[parts.length - 1]; 
+        //this line return only 'css' and 'js'
+        //console.log(fileExtension);
 
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write('not found '); //response body
-        response.end(); // finish response
-
-
+        fs.readFile(__dirname + '/../public' + endpoint, function (error, file) {
+            if (error) {
+                response.writeHead(500, {'Content-Type': 'text/html'});
+                response.end('<h1>Internel Server Error</h1>');
+            } else {
+                response.writeHead(200, {'Content-Type': ContentTypes[fileExtension]});
+                response.end(file);
+            }
+        });
     }
 
 }
