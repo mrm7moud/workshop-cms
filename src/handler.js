88b362283115ceb4659dfbd5459f4919a9f6e67c
mrm7moud <mrm7moud@hotmail.com>
2017-07-31 11:36:04 +0300

    create handlers.js file

---
+++ src/handler.js
@@ -0,0 +1,72 @@
+var fs = require('fs');
+
+var ContentTypes = {
+    css: 'text/css',
+    js: 'application/javascript',
+    jpg: 'image/jpg',
+    ico: 'image/x-icon',
+    png: 'image/png'
+};
+
+function handleHomeRoute(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+
+    fs.readFile(__dirname + '/../public/index.html', function (error, file) {
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        response.end(file);
+    });
+
+}
+
+function handlePublic(request, response) {
+    var endpoint = request.url;
+    var parts = endpoint.split('.');
+
+    // this line return 'main', 'css' and 'index', 'js'
+    //console.log(parts[0]);
+    var fileExtension = parts[parts.length - 1];
+    //this line return only 'css' and 'js'
+    //console.log(fileExtension);
+
+    fs.readFile(__dirname + '/..' + endpoint, function (error, file) {
+        if (error) {
+            response.writeHead(500, {'Content-Type': 'text/html'});
+            response.end('<h1>Internel Server Error</h1>');
+        } else {
+            response.writeHead(200, {
+                'Content-Type': ContentTypes[fileExtension]
+            });
+            response.end(file);
+        }
+    });
+}
+
+function handleRedirect(request, response){
+      response.writeHead(302,{'Location': '/'})
+        var allTheData = '';
+        request.on('data', function (chunkOfData) {
+            allTheData += chunkOfData;
+        });
+        request.on('end', function () {
+            var convertedData = querystring.parse(allTheData);
+            console.log(convertedData);
+            response.end();
+        });
+}
+
+function handleNotFound(request, response) {
+    response.writeHead(404, {'Content-Type': 'text/html'});
+    response.end('<h1>Not Found</h1>');
+}
+
+
+module.exports = {
+  handleHomeRoute: handleHomeRoute,
+  handlePublic: handlePublic,
+  handleNotFound: handleNotFound,
+  handleRedirect: handleRedirect
+};
--- src/server.js
+++ src/server.js
@@ -1,72 +1,24 @@
 var http = require('http');
-var fs = require('fs');
 var querystring = require('querystring');
-
-var ContentTypes = {
-    css: 'text/css',
-    js: 'application/javascript',
-    jpg: 'image/jpg',
-    ico: 'image/x-icon',
-    png: 'image/png'
-};
+var handlers = require('./handler.js');
 
 var message = 'I am so happy to be part of the Node Girls workshop!';
 
 function handler(request, response) {
+
     var endpoint = request.url;
-    var method = request.method;
+    console.log(endpoint);
+    //var method = request.method;
     //console.log(method);
 
     if (endpoint === '/') {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-
-        fs.readFile(__dirname + '/../public/index.html', function (error, file) {
-            if (error) {
-                console.log(error);
-                return;
-            }
-
-            response.end(file);
-        });
-
-    } else if (endpoint === '/node') {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(message); //response body
-        response.end(); // finish response
-    } else if (endpoint === '/girls') {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(message); //response body
-        response.end(); // finish response
+      handlers.handleHomeRoute(request, response);
     } else if(endpoint === '/create-post'){
-        response.writeHead(302,{'Location': '/'})
-        var allTheData = '';
-        request.on('data', function (chunkOfData) {
-            allTheData += chunkOfData;
-        });
-        request.on('end', function () {
-            var convertedData = querystring.parse(allTheData);
-            console.log(convertedData);
-            response.end();
-        });
-
-    }else {
-        //var url = request.url;
-        var parts = endpoint.split('.'); 
-        // this line return 'main', 'css' and 'index', 'js'
-        //console.log(parts[0]);
-        var fileExtension = parts[parts.length - 1]; 
-        //this line return only 'css' and 'js'
-        //console.log(fileExtension);
-
-        fs.readFile(__dirname + '/../public' + endpoint, function (error, file) {
-            if (error) {
-                response.writeHead(500, {'Content-Type': 'text/html'});
-                response.end('<h1>Internel Server Error</h1>');
-            } else {
-                response.writeHead(200, {'Content-Type': ContentTypes[fileExtension]});
-                response.end(file);
-            }
-        });
+       handlers.handleRedirect(request, response);
+    }else if(endpoint.startsWith('/public')){
+        handlers.handlePublic(request, response);
+    }else{
+        handlers.handleNotFound(request, response);
     }
 
 }
